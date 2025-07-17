d9a6178fa7105c33a7390aa00a696babc73db8a1
Silvio Traversaro <silvio@traversaro.it>
2025-07-17 14:31:10 +0200

    Handle stack_size_to_use parameter

--- src/slam_toolbox_async_node.cpp
+++ src/slam_toolbox_async_node.cpp
@@ -29,6 +29,11 @@ int main(int argc, char ** argv)
     auto temp_node = std::make_shared<rclcpp::Node>("slam_toolbox");
     temp_node->declare_parameter("stack_size_to_use",rclcpp::ParameterType::PARAMETER_INTEGER );
     if (temp_node->get_parameter("stack_size_to_use", stack_size)) {
+#ifdef _WIN32
+     if (stack_size != 40'000'000) {
+        RCLCPP_WARN(temp_node->get_logger(), "Can't dynamically change stack size on Windows to %i. Node using stack size 40000000", (int)stack_size);
+      }
+#else
       RCLCPP_INFO(temp_node->get_logger(), "Node using stack size %i", (int)stack_size);
       const rlim_t max_stack_size = stack_size;
       struct rlimit stack_limit;
@@ -37,6 +42,7 @@ int main(int argc, char ** argv)
         stack_limit.rlim_cur = stack_size;
       }
       setrlimit(RLIMIT_STACK, &stack_limit);
+#endif
     }
   }
 
